64067b4abd1d9080fbe956c9dae7ee2dc84efd35
Tomas3w <88631406+Tomas3w@users.noreply.github.com>
2023-10-10 21:26:12 -0300

    Cambio algo

--- Assets/Scripts/GearController.cs
+++ Assets/Scripts/GearController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class GearController : MonoBehaviour
 {
-    public float x_rotation;
-    public float y_rotation;
+    public Queue<Vector2> animacion = new Queue<Vector2>();
+    public float velocidad_proceso = 1;
+
+    public Vector2 rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,32 @@ public class GearController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles = new Vector3(x_rotation, y_rotation, 0);
+        transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, 180);
+        if (animacion.Count != 0)
+        {
+            rotation = Vector2.MoveTowards(rotation, animacion.Peek(), Time.deltaTime);
+        }
+    }
+
+    float ObtenerCambioX(int cambio)
+    {
+        return 7;
+    }
+
+    float ObtenerCambioY(int cambio)
+    {
+        return 7;
+    }
+
+    void HacerCambio(int cambio_actual, int cambio_nuevo)
+    {
+        animacion.Clear();
+        animacion.Enqueue(new Vector2(ObtenerCambioX(cambio_nuevo), ObtenerCambioY(cambio_nuevo)));
+        // int cambio_actual_col = ColumnaDeCambio(cambio_actual);
+        // int cambio_nuevo_col = ColumnaDeCambio(cambio_nuevo);
+        // if (cambio_actual_col == cambio_nuevo_col)
+        // {
+        //     //
+        // }
     }
 }
